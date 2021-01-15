a16dab0311e5e5c92b248021766311988e90f067
Naggeon Sung <nk.sung@gmail.com>
2021-01-16 02:35:06 +0900

    member 데이터 생성 및 security 등록

---
+++ src/main/java/me/myclude/calculator/common/controller/AccountController.java
@@ -0,0 +1,21 @@
+package me.myclude.calculator.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+	
+	@GetMapping("/login")
+	public String login() {
+		return "account/login";
+	}
+	
+	@GetMapping("/register")
+	public String Register() {
+		return "account/register";
+	}
+}
